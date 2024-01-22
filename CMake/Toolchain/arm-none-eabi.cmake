1237af306ae170318bd3cbd099ed9ebd560d3eb9
meshkov <maximesh17@gmail.com>
2024-01-23 01:52:11 +0300

    add condition for compatibility with other toolchain locations

--- CMake/Toolchain/arm-none-eabi.cmake
+++ CMake/Toolchain/arm-none-eabi.cmake
@@ -1,4 +1,12 @@
-set(TOOLCHAIN_DIR "C:/Program Files (x86)/GNU Arm Embedded Toolchain/10 2021.10")
+if(EXISTS "C:/Program Files (x86)/GNU Arm Embedded Toolchain/10 2021.10")
+    set(TOOLCHAIN_DIR "C:/Program Files (x86)/GNU Arm Embedded Toolchain/10 2021.10")
+elseif(EXISTS "C:/GNU Arm Embedded Toolchain/10 2021.10")
+    set(TOOLCHAIN_DIR "C:/GNU Arm Embedded Toolchain/10 2021.10")
+else()
+    message(FATAL_ERROR "Toolchain not found!")
+endif()
+
+message("TOOLCHAIN_DIR: ${TOOLCHAIN_DIR}")
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_VERSION 1)
@@ -11,4 +19,3 @@ set(TOOLCHAIN_TRIPLET "arm-none-eabi")
 set(CMAKE_LIBRARY_ARCHITECTURE arm-none-eabi)
 
 include(${CMAKE_CURRENT_LIST_DIR}/gcc.cmake)
-
