3f2777784fd74f398489311edfb8af2c985c3010
vgtutf081 <vgtutf081@gmail.com>
2024-05-13 13:44:39 +0300

    add flash fm25v10, add blocking non blocking mode spi

--- Driver/Spi/SpiMaster.h
+++ Driver/Spi/SpiMaster.h
@@ -10,9 +10,14 @@ namespace ES::Driver {
 
     struct ISpiMaster {
         virtual ~ISpiMaster() = default;
-        virtual void setEventHandler(ISpiEventHandler* handler) = 0;
+        virtual bool setEventHandler(ISpiEventHandler* handler) = 0;
         virtual void read(uint8_t* rxBuffer, size_t size) = 0;
         virtual void write(const uint8_t* txBuffer, size_t size) = 0;
         virtual bool readWrite(const uint8_t* txBuffer, uint8_t* rxBuffer, size_t size) = 0;
+
+        enum SpiMode {
+            Blocking,
+            NonBlocking
+        };
     };
 }
