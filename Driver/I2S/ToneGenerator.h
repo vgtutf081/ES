50d62c5aea3c9f5caee86cfb5dd99502f6c6292a
Maxim Khudiakov <mkhudiakov@sitronics.com>
2024-06-03 19:06:34 +0300

    Fix tone generator

--- Driver/I2S/ToneGenerator.h
+++ Driver/I2S/ToneGenerator.h
@@ -18,16 +18,22 @@ namespace ES::Driver::Tone {
         }
 
         const std::vector<uint32_t>& generateTone(Tone tone, uint32_t freq, float amplitude, uint32_t offset, I2S::SampleRate sampleRate) {
-            float sampleTime = 1000000 / static_cast<float>(sampleRate);
-            float seqTime = 1000000 / freq;
-            size_t PartSize = seqTime / sampleTime;
-            if(tone == Tone::Sine) {
-                float angle = 0.f;
-                uint32_t a = offset * amplitude;
-                for(size_t i = 0; i < PartSize; i++) {
-                    vec.push_back(uint32_t((a * sin(angle)) + offset));
-                    angle += (2.f * (float)M_PI) / PartSize;
+            size_t PartSize = static_cast<float>(sampleRate) / freq;
+            float fixedAmplitude = offset * amplitude;
+            float value = 0.0f;
+            vec.clear();
+            for (size_t frame = 0; frame < PartSize; ++frame) {
+                float pos = fmod(freq * frame / static_cast<float>(sampleRate), 1.0f);
+                if (tone == Tone::Sine) {
+                    value = sin(pos * 2 * M_PI);
                 }
+                else if (tone == Tone::SawTooth) {
+                    value = pos * 2 - 1;
+                }
+                else if (tone == Tone::Triangle) {
+                    value = 1 - fabs(pos - 0.5) * 4;
+                }
+                vec.push_back(static_cast<uint32_t>(value * fixedAmplitude + offset));
             }
             return vec;
         }
